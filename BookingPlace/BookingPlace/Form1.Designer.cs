f8f8883b9ba6542924b145c46b402d391b341011
SaltyMox <50733209+GreenMad1235@users.noreply.github.com>
2021-05-30 13:27:37 +0300

    BookingPlace

---
+++ BookingPlace/BookingPlace/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace BookingPlace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 268);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(746, 148);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(529, 162);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(241, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(529, 213);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(241, 22);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(506, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "С";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(496, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "До";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(529, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(529, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Фамилия";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BookingPlace/BookingPlace/Form1.cs
@@ -0,0 +1,103 @@
+﻿using BookingPlace.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BookingPlace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Draw_Button();
+
+
+        }
+
+        /*________________________________________________________DRAW_________________________________________*/
+        private void Draw_Button()
+        {
+            const int x = 15, y = 25;
+            Room r = new Room();
+            Button[,] buttons = new Button[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    buttons[i, j] = new Button()
+                    {
+                           
+                        Size = new Size(15, 15),
+                        Location = new Point(18 + j * 14, 5 + i * 14),
+                        BackColor = Color.Green,
+                        Tag = new Point(i, j)
+                    };
+                    buttons[i,j].Click += new EventHandler(buttons_click);
+                    this.Controls.Add(buttons[i, j]);
+        
+                }
+
+            }
+        }
+        
+        /*_____________________________________________________OUTPUT/INPUT________________________________________________*/
+        private void SetInDb(Button b)
+        {
+            using(var db = new RoomContext())
+            {
+                Room r = new Room()
+                {
+                    Number = b.TabIndex.ToString(),
+                    Name = textBox1.Text,
+                    Surname = textBox2.Text,
+                    Status = false,
+                    Date = dateTimePicker1.Value,
+                    LastTime = dateTimePicker2.Value
+
+                };
+
+                
+
+                if(r.Name.Length == 0 && r.Surname.Length == 0)
+                {
+                    MessageBox.Show("Enter name or surname");
+                }
+                else
+                {
+                    b.BackColor = Color.Red;
+                    db.Rooms.Add(r);
+                    db.SaveChanges();
+                    richTextBox1.Text += "Номер: " + r.Number + " Имя "+ r.Name + " Фамилия " + r.Surname + " Статус комнаты: " + r.Status + " Дата бронирования: " + r.Date
+                        + "Дата окончания " + r.LastTime;
+                }
+                
+            }
+
+        }
+        /*____________________________________________________________________________________________________________*/
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void buttons_click(object sender, EventArgs e) 
+        {
+            Button b = sender as Button;
+            SetInDb(b);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
