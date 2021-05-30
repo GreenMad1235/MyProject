3bfc6490c57b2553292a0e3c98185c51547345a9
SaltyMox <50733209+GreenMad1235@users.noreply.github.com>
2021-05-30 13:29:12 +0300

    Add files via upload

---
+++ BookingPlace/BookingPlace/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingPlace
+{
+    class Room
+    {
+        //public Class Class { get; set; }
+        public int RoomID { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public DateTime LastTime { get; set; }
+        public int Price { get; set; } = 1500;
+        public bool Status { get; set; } = true;
+
+
+    }
+}
